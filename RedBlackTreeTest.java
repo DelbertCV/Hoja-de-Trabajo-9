911e35fc79a330f58b851a8ebcc723b654819a4b
jonagr7 <agu14349@uvg.edu.gt>
2015-10-18 00:06:04 -0600

    Update and rename RedBlackTree2Test.java to RedBlackTreeTest.java

---
+++ RedBlackTreeTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RedBlackTreeTest {
+
+	@Test
+	public void test() {
+		RedBlackTree arbol = new RedBlackTree();
+		Word a = new Word();
+		arbol.add(a); arbol.get(a);
+	}
+
+}
