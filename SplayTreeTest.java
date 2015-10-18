4488c0190eafc4258b5b9844ba1742c7269494c9
jonagr7 <agu14349@uvg.edu.gt>
2015-10-18 00:07:25 -0600

    Update and rename SplayTree2Test.java to SplayTreeTest.java

---
+++ SplayTreeTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SplayTreeTest {
+
+	@Test
+	public void test() {
+		SplayTree arbol = new SplayTree();
+		Word a = new Word();
+		arbol.add(a); arbol.get(a);
+	}
+
+}
