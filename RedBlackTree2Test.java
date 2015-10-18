2fd0593aa828ff7540632f2801b4ef788dfd0118
DelbertCV <dfcustodiovargas@gmail.com>
2015-10-17 23:50:49 -0600

    Junits

---
+++ RedBlackTree2Test.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RedBlackTree2Test {
+
+	@Test
+	public void test() {
+		RedBlackTree2 red = new RedBlackTree2();
+		Word a = new Word();
+		red.add(a);
+		red.get(a);
+
+	}
+
+}
---
+++ SplayTree2Test.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SplayTree2Test {
+
+	@Test
+	public void test() {
+		SplayTree2 splay = new SplayTree2();
+		Word a = new Word();
+		splay.add(a);
+		splay.get(a);
+	}
+
+}
